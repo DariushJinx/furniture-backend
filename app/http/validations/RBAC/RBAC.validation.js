59cf0ffc9a8f2de71d837bc3073cdd32b3463a47
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-24 02:20:29 +0330

    validations  for roles and permissions added

---
+++ app/http/validations/RBAC/RBAC.validation.js
@@ -0,0 +1,34 @@
+const Joi = require("@hapi/joi");
+const createHttpError = require("http-errors");
+const { MongoIDPattern } = require("../../../utils/constans.utils");
+
+const RoleSchema = Joi.object({
+  title: Joi.string()
+    .min(3)
+    .max(30)
+    .error(createHttpError.BadRequest("عنوان وارد شده صحیح نمی باشد")),
+  description: Joi.string()
+    .min(0)
+    .max(30)
+    .error(createHttpError.BadRequest("توضیحات وارد شده نقش صحیح نمی باشد")),
+  permissions: Joi.array()
+    .items(Joi.string().pattern(MongoIDPattern))
+    .error(createHttpError.BadRequest("دسترسی های  وارد شده صحیح نمی باشد")),
+});
+const PermissionSchema = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .error(createHttpError.BadRequest("نام وارد شده صحیح نمی باشد")),
+  description: Joi.string()
+    .min(0)
+    .max(30)
+    .error(createHttpError.BadRequest("توضیحات وارد شده نقش صحیح نمی باشد")),
+});
+
+const RBACValidation = {
+  RoleSchema,
+  PermissionSchema,
+};
+
+module.exports = RBACValidation;
