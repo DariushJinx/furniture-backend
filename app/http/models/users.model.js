27a2f3840576cc04e3a468536f8d7502e81a4057
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-24 02:18:01 +0330

    model  for authentication and users added

---
+++ app/http/models/users.model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  productID: { type: mongoose.Types.ObjectId, ref: "products" },
+  count: { type: Number, default: 1 },
+});
+
+const BasketSchema = new mongoose.Schema({
+  products: { type: [ProductSchema], default: [] },
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String },
+    last_name: { type: String },
+    username: { type: String, lowercase: true },
+    mobile: { type: String, required: true, unique: true },
+    email: { type: String, lowercase: true },
+    password: { type: String },
+    otp: {
+      type: Object,
+      default: {
+        code: 0,
+        expiresIn: 0,
+      },
+    },
+    bills: { type: [], default: [] },
+    discount: { type: Number, default: 0 },
+    birthday: { type: String },
+    token: { type: String, default: "" },
+    refreshToken: { type: String, default: "" },
+    Role: { type: String, default: "USER" },
+    Products: { type: [mongoose.Types.ObjectId], ref: "products", default: [] },
+    Blogs: { type: [mongoose.Types.ObjectId], ref: "blogs", default: [] },
+    basket: { type: BasketSchema },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.index({
+  first_name: "text",
+  last_name: "text",
+  username: "text",
+  mobile: "text",
+  email: "text",
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
