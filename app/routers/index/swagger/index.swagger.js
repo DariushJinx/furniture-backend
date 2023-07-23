a7248337e082136ea64cba36de84a1e736b16dc4
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-24 02:21:49 +0330

    swagger  for home or index added

---
+++ app/routers/index/swagger/index.swagger.js
@@ -0,0 +1,16 @@
+/**
+ * @swagger
+ *  / :
+ *      get :
+ *          tags : [HomePage]
+ *          summary : index page
+ *          description : this page is home page
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  schema :
+ *                      type : string
+ *                      example : index page store
+ *              400 :
+ *                  description : not found
+ */
