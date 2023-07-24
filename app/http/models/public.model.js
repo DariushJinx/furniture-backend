baeffcfebbdd7175d5393d8c467c10dada92f3ef
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-24 23:03:07 +0330

    create and complete public model

---
+++ app/http/models/public.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const AnswerSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    comment: { type: String, required: true },
+    show: { type: Boolean, required: true, default: false },
+    openToComment: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CommentSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    comment: { type: String, required: true },
+    show: { type: Boolean, required: true, default: false },
+    openToComment: { type: Boolean, default: true },
+    answers: { type: [AnswerSchema], default: [] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const publicModel = {
+    CommentSchema,
+};
+
+module.exports = publicModel;
