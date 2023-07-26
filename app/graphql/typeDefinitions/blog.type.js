39f764bb95c5c743000ef771b0b0cfe90f8b0859
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-26 23:48:15 +0330

    create and complete type for blog in graphql

---
+++ app/graphql/typeDefinitions/blog.type.js
@@ -0,0 +1,24 @@
+const { GraphQLObjectType, GraphQLString, GraphQLList } = require("graphql");
+const { UserType, PublicCategoryType } = require("./public.type");
+const CommentType = require("./comment.type");
+
+const BlogType = new GraphQLObjectType({
+  name: "BlogType",
+  fields: {
+    _id: { type: GraphQLString },
+    author: { type: UserType },
+    title: { type: GraphQLString },
+    text: { type: GraphQLString },
+    short_text: { type: GraphQLString },
+    image: { type: GraphQLString },
+    imageURL: { type: GraphQLString },
+    tags: { type: new GraphQLList(GraphQLString) },
+    category: { type: PublicCategoryType },
+    comments: { type: new GraphQLList(CommentType) },
+    likes: { type: new GraphQLList(UserType) },
+    dislikes: { type: new GraphQLList(UserType) },
+    bookmarks: { type: new GraphQLList(UserType) },
+  },
+});
+
+module.exports = BlogType;
