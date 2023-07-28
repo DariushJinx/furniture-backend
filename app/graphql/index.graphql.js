befd6c84c70f5679e9e94ed1bb0871f80fb1b438
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-28 22:47:43 +0330

    update grphql,query and mutation

--- app/graphql/index.graphql.js
+++ app/graphql/index.graphql.js
@@ -1,9 +1,36 @@
 const { GraphQLObjectType, GraphQLSchema } = require("graphql");
-const BlogResolver = require("./queries/blog.resolver");
+
+const {
+  createCommentForBlog,
+  createCommentForProduct,
+} = require("./mutations/comment.resolver");
+const {
+  BookmarkProduct,
+  BookmarkBlog,
+} = require("./mutations/bookmark.resolver");
+const { LikedProduct, LikedBlog } = require("./mutations/likes.resolver");
+const {
+  DislikedBlog,
+  DislikedProduct,
+} = require("./mutations/dislikes.resolver");
+const {
+  AddProductToBasket,
+  RemoveProductFromBasket,
+  RemoveAllProducts,
+} = require("./mutations/basket.resolver");
+
 const {
   CategoryResolver,
   CategoryChildResolver,
 } = require("./queries/category.resolver");
+const {
+  getUserBookmarkedBlogs,
+  getUserBookmarkedProduct,
+  getUserBasket,
+} = require("./queries/user-profile.resolver");
+
+const BlogResolver = require("./queries/blog.resolver");
+
 const ProductResolver = require("./queries/product.resolver");
 
 const RootQuery = new GraphQLObjectType({
@@ -13,16 +40,31 @@ const RootQuery = new GraphQLObjectType({
     products: ProductResolver,
     categories: CategoryResolver,
     childOfCategory: CategoryChildResolver,
+    getUserBookmarkedBlogs,
+    getUserBookmarkedProduct,
+    getUserBasket,
   },
 });
 const RootMutations = new GraphQLObjectType({
   name: "RootMutations",
-  fields: {},
+  fields: {
+    createCommentForBlog,
+    createCommentForProduct,
+    BookmarkProduct,
+    BookmarkBlog,
+    LikedProduct,
+    LikedBlog,
+    DislikedBlog,
+    DislikedProduct,
+    AddProductToBasket,
+    RemoveProductFromBasket,
+    RemoveAllProducts,
+  },
 });
 
 const QLSchema = new GraphQLSchema({
   query: RootQuery,
-  // mutation: RootMutations,
+  mutation: RootMutations,
 });
 
 module.exports = QLSchema;
