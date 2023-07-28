40d4954bbe248b80d0725b0ed84f4a66275d2e8e
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-07-28 22:58:32 +0330

    file updated

--- app/routers/admin/main.admin/main.admin.routes.js
+++ app/routers/admin/main.admin/main.admin.routes.js
@@ -3,6 +3,7 @@ const categoryRoutes = require("../category.routes");
 const permissionRoutes = require("../permisson.routes");
 const productRoutes = require("../product.routes");
 const roleRotes = require("../role.routes");
+const userRoutes = require("../user.routes");
 
 const adminRoutes = require("express").Router();
 
@@ -11,5 +12,6 @@ adminRoutes.use("/role", roleRotes);
 adminRoutes.use("/blogs", blogRoutes);
 adminRoutes.use("/category", categoryRoutes);
 adminRoutes.use("/product", productRoutes);
+adminRoutes.use("/users", userRoutes);
 
 module.exports = adminRoutes;
